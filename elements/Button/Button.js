c84b77caafc6af6442e970d94bce8d668d829856
codex-zira <zirah@codex.works>
2022-06-14 13:37:57 +0700

    Update some component =

--- elements/Button/Button.js
+++ elements/Button/Button.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './styles.module.css';
 
-const buttonStyle = {
-  backgroundColor: 'blue',
-  color: 'white',
-  border: 'none',
-  padding: '1.6rem 8rem'
-}
-
-const Button = ({ children, style, ...rest }) => <button {...rest} style={{...buttonStyle, ...style}} >{children}</button>;
+const Button = ({ children, className, ...rest }) => <button {...rest} className={styles['button-elem'] + ' ' + className} >{children}</button>;
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
--- elements/TextWithButton/TextWithButton.js
+++ elements/TextWithButton/TextWithButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '../Button';
 import Text from '../Text';
+import PropTypes from 'prop-types';
 
 const groupStyle = {
   display: 'flex',
